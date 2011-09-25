955a19fe6947afcbe51fe526747a3bad6a55c02f
Jeff Squires <jeff.squires@gmail.com>
2011-09-24 20:21:14 -0400

    remove load_resource_instance as it was loading even on 'new' and preventing adding a new variant

--- app/controllers/admin/variants_controller_decorator.rb
+++ app/controllers/admin/variants_controller_decorator.rb
@@ -5,11 +5,6 @@ Admin::VariantsController.class_eval do
     :success => lambda { redirect_to(@variant.is_master ? volume_prices_admin_product_variant_url(@variant.product, @variant) : collection_url) },
     :failure => lambda { redirect_to(@variant.is_master ? volume_prices_admin_product_variant_url(@variant.product, @variant) : collection_url)  } } }
 
-  def load_resource_instance
-    parent
-    Variant.find(params[:id])
-  end
-
   def volume_prices
     @product = @variant.product
   end
