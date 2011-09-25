6d32ba8018f50ec972b7c8232c0fbd87689685eb
Jeff Squires <jeff.squires@gmail.com>
2011-09-25 09:47:31 -0400

    tweak last commit.  make sure the variants controller correctly handles variant loading for master & non-master

--- app/controllers/admin/variants_controller_decorator.rb
+++ app/controllers/admin/variants_controller_decorator.rb
@@ -5,6 +5,16 @@ Admin::VariantsController.class_eval do
     :success => lambda { redirect_to(@variant.is_master ? volume_prices_admin_product_variant_url(@variant.product, @variant) : collection_url) },
     :failure => lambda { redirect_to(@variant.is_master ? volume_prices_admin_product_variant_url(@variant.product, @variant) : collection_url)  } } }
 
+  def load_resource_instance
+    parent
+
+    if new_actions.include?(params[:action].to_sym)
+      build_resource
+    elsif params[:id]
+      Variant.find(params[:id])
+    end
+  end
+
   def volume_prices
     @product = @variant.product
   end
