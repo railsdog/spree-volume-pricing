c747070de20aa7ff47d6f633e2042d2b5dd94c85
Adam Anderson <adamandersonis@gmail.com>
2012-04-30 17:30:32 -0700

    Make compatible with Rails 3.2 mass assignment and nested attributes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 group :test do
+  gem 'pry'
   gem 'rspec-rails', '~> 2.9.0'
   gem 'factory_girl', '~> 2.6.4'
   gem 'factory_girl_rails', '~> 1.7'
--- spec/controllers/spree/admin/variants_controller_spec.rb
+++ spec/controllers/spree/admin/variants_controller_spec.rb
@@ -7,13 +7,25 @@ describe Spree::Admin::VariantsController do
     user.stub :has_role? => true
   end
 
-  context "#update" do
-    it "should fire the before filter" do
-      variant = Factory(:variant)
+  describe "PUT #update" do
+    it "creates a volume price" do
+      @variant = Factory :variant, :price => 10
+      put :update,
+        :product_id => @variant.product.permalink,
+        :id => @variant.id,
+        :variant => {
+          "volume_prices_attributes" => {
+            "1335830259720" => {
+              "name"=>"5-10",
+              "range"=>"5..10",
+              "amount"=>"90",
+              "position"=>"1",
+              "_destroy"=>"false"
+            }
+          }
+        }
 
-      controller.should_receive(:before_update)
-
-      put :update, {:product_id=>variant.product.permalink, :id => variant.id,:variant => variant.attributes}
+      Spree::VolumePrice.last.name.should eql("5-10")
     end
   end
 end
