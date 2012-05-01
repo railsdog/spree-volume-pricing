cfe98e8d68d9d156e713f7b7fc9b153616edb881
Adam Anderson <adamandersonis@gmail.com>
2012-04-30 17:41:39 -0700

    Make Spree::Admin::VariantsController spec less dumb

--- spec/controllers/spree/admin/variants_controller_spec.rb
+++ spec/controllers/spree/admin/variants_controller_spec.rb
@@ -9,23 +9,24 @@ describe Spree::Admin::VariantsController do
 
   describe "PUT #update" do
     it "creates a volume price" do
-      @variant = Factory :variant, :price => 10
-      put :update,
-        :product_id => @variant.product.permalink,
-        :id => @variant.id,
-        :variant => {
-          "volume_prices_attributes" => {
-            "1335830259720" => {
-              "name"=>"5-10",
-              "range"=>"5..10",
-              "amount"=>"90",
-              "position"=>"1",
-              "_destroy"=>"false"
+      variant = Factory :variant
+
+      lambda do
+        put :update,
+          :product_id => variant.product.permalink,
+          :id => variant.id,
+          :variant => {
+            "volume_prices_attributes" => {
+              "1335830259720" => {
+                "name"=>"5-10",
+                "range"=>"5..10",
+                "amount"=>"90",
+                "position"=>"1",
+                "_destroy"=>"false"
+              }
             }
           }
-        }
-
-      Spree::VolumePrice.last.name.should eql("5-10")
+      end.should change(variant.volume_prices, :count).by(1)
     end
   end
 end
