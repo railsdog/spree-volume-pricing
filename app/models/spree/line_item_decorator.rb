1b4a37a7a95836e7b35f4009b51bb8f3bb779bc7
Michael Bianco <iloveitaly@gmail.com>
2012-08-03 16:22:01 -0400

    'Subclass' the copy_price instead of override

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,13 +1,23 @@
 Spree::LineItem.class_eval do
-  # UPGRADE_CHECK this overrides an existing method on LineItem
-  def copy_price
-    if variant and (
-      price.nil? or (
-        changed? &&
-        changes.keys.include?("quantity")
-      )
-    )
-      self.price = variant.volume_price(quantity)
+  # pattern grabbed from: http://stackoverflow.com/questions/4470108/
+
+  # the idea here is compatibility with spree_sale_products
+  # trying to create a 'calculation stack' wherein the best valid price is
+  # chosen for the product. This is mainly for compatibility with spree_volume_pricing
+
+  old_copy_price = instance_method(:copy_price)
+  define_method(:copy_price) do
+    new_price = old_copy_price.bind(self).()
+
+    if variant and changed? and changes.keys.include? 'quantity'
+      vprice = self.variant.volume_price self.quantity
+      self.price = vprice if (!new_price.nil? and vprice < new_price) or vprice < self.price
+    else
+      if new_price.nil?
+        self.price = self.variant.price
+      else
+        self.price = new_price
+      end
     end
   end
 end
