c011d8bf5efd4fc8b69bb9a71640ee5bfe7261ff
Michael Bianco <iloveitaly@gmail.com>
2012-10-10 18:38:12 -0400

    Code cleanup that was lost during master rebase

--- spec/controllers/spree/admin/variants_controller_spec.rb
+++ spec/controllers/spree/admin/variants_controller_spec.rb
@@ -5,27 +5,29 @@ describe Spree::Admin::VariantsController do
   before do
     controller.stub :spree_current_user => user
     user.stub :has_role? => true
+    controller.stub :admin_variants_url => "/"
   end
 
   describe "PUT #update" do
     it "creates a volume price" do
-      @variant = Factory :variant, :price => 10
-      put :update,
-        :product_id => @variant.product.permalink,
-        :id => @variant.id,
-        :variant => {
-          "volume_prices_attributes" => {
-            "1335830259720" => {
-              "name"=>"5-10",
-              "range"=>"5..10",
-              "amount"=>"90",
-              "position"=>"1",
-              "_destroy"=>"false"
+      variant = FactoryGirl.create :variant
+
+      lambda do
+        spree_put :update,
+          :product_id => variant.product.permalink,
+          :id => variant.id,
+          :variant => {
+            "volume_prices_attributes" => {
+              "1335830259720" => {
+                "name"=>"5-10",
+                "range"=>"5..10",
+                "amount"=>"90",
+                "position"=>"1",
+                "_destroy"=>"false"
+              }
             }
           }
-        }
-
-      Spree::VolumePrice.last.name.should eql("5-10")
+      end.should change(variant.volume_prices, :count).by(1)
     end
   end
 end
--- spec/models/spree/volume_price_spec.rb
+++ spec/models/spree/volume_price_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Spree::VolumePrice do
+  it { should belong_to(:variant) }
+  it { should validate_presence_of(:variant) }
+  it { should validate_presence_of(:amount) }
+
   before(:each) do
     @volume_price = Spree::VolumePrice.new(:variant => Spree::Variant.new, :amount => 10)
   end
