882342ee2ea19fd7a4cb22a70c254332f276d370
Jeff Dutil <jdutil@burlingtonwebapps.com>
2012-11-14 23:03:55 -0500

    Fix for applications that have already run prefix migration that was renamed.

--- db/migrate/20111206173307_prefix_volume_pricing_table_names.rb
+++ db/migrate/20111206173307_prefix_volume_pricing_table_names.rb
@@ -1,6 +1,6 @@
 class PrefixVolumePricingTableNames < ActiveRecord::Migration
   def change
-    rename_table :volume_prices, :spree_volume_prices
+    rename_table :volume_prices, :spree_volume_prices unless Spree::VolumePrice.table_exists?
   end
 end
 
