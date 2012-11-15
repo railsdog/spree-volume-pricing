f16ec7fa618513f378870cb1b9447ec792ce083e
Jeff Dutil <jdutil@burlingtonwebapps.com>
2012-11-15 01:15:08 -0500

    Fix to allow adding volume prices on the edit page.

--- app/controllers/spree/admin/variants_controller_decorator.rb
+++ app/controllers/spree/admin/variants_controller_decorator.rb
@@ -1,4 +1,17 @@
 Spree::Admin::VariantsController.class_eval do
+
+  def edit
+    @variant.volume_prices.build if @variant.volume_prices.empty?
+    super
+  end
+
+  def volume_prices
+    @product = @variant.product
+    @variant.volume_prices.build if @variant.volume_prices.empty?
+  end
+
+  private
+
   # this loads the variant for the master variant volume price editing
   def load_resource_instance
     parent
@@ -10,11 +23,6 @@ Spree::Admin::VariantsController.class_eval do
     end
   end
 
-  def volume_prices
-    @product = @variant.product
-    @variant.volume_prices.build
-  end
-
   def location_after_save
     if @product.master.id == @variant.id and params[:variant].has_key? :volume_prices_attributes
       return volume_prices_admin_product_variant_url(@product, @variant)
@@ -22,4 +30,5 @@ Spree::Admin::VariantsController.class_eval do
 
     super
   end
+
 end
