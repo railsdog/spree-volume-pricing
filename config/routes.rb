19a503444beb940bc0c12d907c57e2cac2c2affe
Tobias Bohwalli <hi@futhr.io>
2015-05-03 17:36:22 +0200

    Remove unnecessary route conditions.
    No worries it supports .add_routes now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
-routes = lambda do
-  namespace :admin do 
-    resources :products do 
+Spree::Core::Engine.add_routes do
+  namespace :admin do
+    resources :products do
        resources :variants do
-        get :volume_prices, :on => :member
+        get :volume_prices, on: :member
       end
     end
 
-    delete '/volume_prices/:id', :to => "volume_prices#destroy", :as => :volume_price
+    delete '/volume_prices/:id', to: 'volume_prices#destroy', as: :volume_price
   end
-end
-
-if Spree::Core::Engine.respond_to?(:add_routes)
-  Spree::Core::Engine.add_routes(&routes)
-else
-  Spree::Core::Engine.routes.draw(&routes)
 end
