932d81f385229e6c28dcb2bcd327ae460a52b23d
Tobias Bohwalli <hi@futhr.io>
2015-05-03 17:28:10 +0200

    Fixes broken install generator path.

---
+++ lib/generators/spree_volume_pricing/install/install_generator.rb
@@ -0,0 +1,24 @@
+module SpreeVolumePricing
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, type: :boolean, default: false
+
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_volume_pricing\n"
+      end
+
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=spree_volume_pricing'
+      end
+
+      def run_migrations
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
+      end
+    end
+  end
+end
