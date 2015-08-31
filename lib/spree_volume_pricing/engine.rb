718bb0bbd1a70337ad839a1beba53fbac6f48f3c
Tobias Bohwalli <hi@futhr.io>
2015-08-31 10:20:09 +0200

    delete shorter then gsub.

--- lib/spree_volume_pricing/engine.rb
+++ lib/spree_volume_pricing/engine.rb
@@ -21,10 +21,10 @@ module SpreeVolumePricing
           case count('.')
           when 2
             elements = split('..')
-            return Range.new(elements[0].gsub('(', '').to_i, elements[1].to_i)
+            return Range.new(elements[0].delete('(').to_i, elements[1].to_i)
           when 3
             elements = split('...')
-            return Range.new(elements[0].gsub('(', '').to_i, elements[1].to_i - 1)
+            return Range.new(elements[0].delete('(').to_i, elements[1].to_i - 1)
           else
             raise ArgumentError.new("Couldn't convert to Range: #{self}")
           end
