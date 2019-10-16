458f9d975a6ff1337e5405c8631ef9d61ee33eb0
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-16 08:07:35 +0200

    Adds spree version variation in line_item_decorator.rb

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -6,21 +6,40 @@ module Spree::LineItemDecorator
   # chosen for the product. This is mainly for compatibility with spree_sale_products
   #
   # Assumption here is that the volume price currency is the same as the product currency
-  def self.prepend(base)
-    old_copy_price = base.instance_method(:copy_price)
+  
+  def self.prepended(base)
+    if Spree.version.to_f >= 3.7
+      old_copy_price = base.instance_method(:copy_price)
 
-    def copy_price
-      old_copy_price.bind(self).call
-      return unless variant
-  
-      if changed? && changes.keys.include?('quantity')
-        vprice = variant.volume_price(quantity, order.user)
-        if price.present? && vprice <= variant.price
+      define_method(:copy_price) do
+        old_copy_price.bind(self).call
+        return unless variant
+
+        if changed? && changes.keys.include?('quantity')
+          vprice = variant.volume_price(quantity, order.user)
+          if price.present? && vprice <= variant.price
           self.price = vprice and return
+          end
         end
+
+        self.price = variant.price if price.nil?
       end
-  
-      self.price = variant.price if price.nil?
+    else
+      old_copy_price = base.instance_method(:copy_price)
+
+      def copy_price
+        old_copy_price.bind(self).call
+        return unless variant
+
+        if changed? && changes.keys.include?('quantity')
+          vprice = variant.volume_price(quantity, order.user)
+          if price.present? && vprice <= variant.price
+          self.price = vprice and return
+          end
+        end
+
+        self.price = variant.price if price.nil?
+      end
     end
   end
 end
