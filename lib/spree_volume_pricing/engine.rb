bacc8dae0987c9088cac9b7848f5b8f6f0ff2dfb
Matthew Kennedy <m.kennedy@me.com>
2020-04-18 19:02:58 +0100

    Add dropshipper option, remove coffee script requirement

--- lib/spree_volume_pricing/engine.rb
+++ lib/spree_volume_pricing/engine.rb
@@ -8,6 +8,7 @@ module SpreeVolumePricing
       Spree::AppConfiguration.class_eval do
         preference :use_master_variant_volume_pricing, :boolean, default: false
         preference :volume_pricing_role, :string, default: 'wholesale'
+        preference :volume_pricing_role_dropship, :string, default: 'dropship'
       end
     end
 
