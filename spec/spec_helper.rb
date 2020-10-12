6baeb8b5c5db6c26aa33021e08c04b45b10df176
Robert Nowakowski <aplegatt@gmail.com>
2020-10-12 13:37:55 +0200

    Update spec_helper and gemspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,36 +1,11 @@
-require 'simplecov'
-SimpleCov.start 'rails'
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
 
-ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
-begin
-  require File.expand_path('../dummy/config/environment', __FILE__)
-rescue LoadError
-  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
-  exit
-end
-
-require 'rspec/rails'
-require 'ffaker'
 require 'shoulda-matchers'
-require 'pry'
+require 'spree_dev_tools/rspec/spec_helper'
 
-RSpec.configure do |config|
-  config.fail_fast = false
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
-
-  config.mock_with :rspec
-  config.use_transactional_fixtures = false
-  config.raise_errors_for_deprecations!
-  config.infer_spec_type_from_file_location!
-
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
-  end
-end
-
-# From: https://github.com/thoughtbot/shoulda-matchers/issues/384
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -38,4 +13,6 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |file| require file }
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].sort.each { |f| require f }
